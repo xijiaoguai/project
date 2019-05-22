55dfa364b7f7a2847880591b5e5d751fa12e0567
liu <2579186091@qq.com>
2019-05-22 20:16:29 +0800

    2019年5月22日20:16:26

---
+++ app/admin/controller/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 25791
+ * Date: 2019/5/19
+ * Time: 15:37
+ */
+
+namespace app\admin\controller;
+use app\admin\base;
+class index extends base
+{
+    /**
+     * @api 首页
+     * @return \think\response\View
+     */
+    public function index()
+    {
+        return view('index',$this->data);
+    }
+}
